4c3c315cee92c3f0f3910ca39eb0cdcdd4c02dae
Dani Alonso <alonsopdani@gmail.com>
2019-06-12 20:23:07 +0200

    skull removal part removed

--- source/code.py
+++ source/code.py
@@ -96,44 +96,7 @@ def preparation(images_y, images_n):
     print('images in gray scale')
     show_images(images_y[images_to_show[0]], images_n[images_to_show[1]], '3-grayscale.png')
 
-    '''
-    # convert all images from gray to color
-    def gray2bgr(list_of_images):
-        return [cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) for img in list_of_images]
-
-    images_y, images_n = img_to_gray_scale(images_y), img_to_gray_scale(images_n)
-
-    # skull removal
-    def skull_removal(pic):
-        gray = cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
-        ret, markers = cv2.connectedComponents(thresh)
-        #Get the area taken by each component. Ignore label 0 since this is the background.
-        marker_area = [np.sum(markers==m) for m in range(np.max(markers)) if m!=0]
-        
-        #Get label of largest component by area
-        largest_component = np.argmax(marker_area)+1 #Add 1 since we dropped zero above                        
-
-        #Get pixels which correspond to the brain
-        brain_mask = markers==largest_component
-        brain_out = pic.copy()
-        #In a copy of the original image, clear those pixels that don't correspond to the brain
-        brain_out[brain_mask==False] = (0,0,0)
-        brain_mask = np.uint8(brain_mask)
-        kernel = np.ones((8,8),np.uint8)
-        closing = cv2.morphologyEx(brain_mask, cv2.MORPH_CLOSE, kernel)
-
-        brain_out = pic.copy()
-        #In a copy of the original image, clear those pixels that don't correspond to the brain
-        brain_out[closing==False] = (0,0,0)
-        return brain_out
-
-    # function to apply the previous one in the whole list
-    def skull_removal_list(list_of_images):
-        return [skull_removal(img) for img in list_of_images]
-
-    images_y, images_n = skull_removal_list(images_y), skull_removal_list(images_n)
-    '''
+    # to do: try to remove skull
     
     # function to apply a filter to soften the images
     def median_filter(list_of_images):
